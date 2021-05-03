f9b9b1a17647fc03672b591b1973340eded7a9e1
Krzysztof Garbos <krzysztof_garbos@hotmail.com>
2021-05-03 09:55:54 -0400

    "Added Projects to Porject Section"

--- src/Components/Projects/projects.js
+++ src/Components/Projects/projects.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import './projects.scss';
 import { motion } from 'framer-motion';
+import Slider from 'react-animated-slider';
+import 'react-animated-slider/build/horizontal.css';
+import project1 from '../../Assets/photo-1548092372-0d1bd40894a3.jfif';
+import project2 from '../../Assets/200310_vod_orig_covid_body_hpMain_16x9_992.jpg';
+
+const content = [
+  { title: 'MERN Email Authentication', description: 'Allows users to login, register and reset their password via email', image: project1, href:"https://mern-auth-email.herokuapp.com/" },
+  { title: 'Covid-19 Tracker', description: 'Tracks daily/total cases of Covid-19 in Canada using ChartJS', image: project2, href:"https://github.com/kgarbos/covid19-app" }
+];
 
 const buttonVariants = {
   hover: {
@@ -37,12 +46,36 @@ const Projects = () => {
       <h1>Projects</h1>
       <div className='line'></div>
       <div>
-          <motion.button
-            variants={buttonVariants}
-            whileHover="hover"
-          >
-            Coming Soon!
-          </motion.button>
+        <motion.button
+          variants={buttonVariants}
+          whileHover="hover"
+          className="repoButton"
+        >
+          <a href='https://github.com/kgarbos?tab=repositories' target="_blank" rel='noopener noreferrer'> 
+            <span className="spanText">See More + Code!</span>
+          </a>
+        </motion.button>
+        <Slider autoplay={3000}>
+          {content.map((item, index) => (
+            <div
+              key={index}
+              style={{ background: `url('${item.image}') no-repeat center center` }}
+            >
+              <div className="center">
+                <h1>{item.title}</h1>
+                <p className="description">{item.description}</p>
+                <motion.button
+                  variants={buttonVariants}
+                  whileHover="hover"
+                >
+                  <a href={item.href} target="_blank" rel='noopener noreferrer'> 
+                    <span className="spanText">See App!</span>
+                  </a>
+                </motion.button>
+              </div>
+            </div>
+          ))}
+        </Slider>
       </div>
     </motion.div>
   )
